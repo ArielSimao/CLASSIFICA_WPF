f3651873824c9adfd4bda0021fb3e3cc2f6aca23
ArielSimao <arielsimao@outlook.com>
2018-03-06 15:25:07 -0300

    Initial commit

---
+++ CLASSIFICA WPF/Cadastro.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CLASSIFICA_WPF
+{
+    /// <summary>
+    /// Lógica interna para Cadastro.xaml
+    /// </summary>
+    public partial class Cadastro : Window
+    {
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MainWindow jan2 = new MainWindow();
+            jan2.Show();
+        }
+
+        private void btnConfirmar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CLASSIFICA WPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CLASSIFICA_WPF
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Cadastro jan2 = new Cadastro();
+            jan2.Show();
+        }
+
+        private void btnAvaliar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Avaliar jan2 = new Avaliar();
+            jan2.Show();
+
+            
+        }
+
+        private void btnRanking_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Ranking jan2 = new Ranking();
+            jan2.Show();
+        }
+    }
+}
